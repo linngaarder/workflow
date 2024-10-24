cc08d3be9865a23791095fec63d20049813aa224
Linn <linngaarder@gmail.com>
2024-10-24 23:07:42 +0200

    Added prettier and eslint to project

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
+
+export default [
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  eslintPluginPrettier,
+  {
+    rules: {
+      "prettier/prettier": [
+        "error",
+        {
+          endOfLine: "auto",
+        },
+      ],
+    },
+  },
+];
